e59d10d6f8897a62f1af3d93c3a0bc598926bbe1
KRUTHIKA C B <kruthikacb26@gmail.com>
2026-03-11 11:39:56 +0530

    Update server.py
    Added event aggregation and ACK response

--- server.py
+++ server.py
@@ -8,13 +8,34 @@ sock.bind((HOST, PORT))
 
 print("Monitoring server started on port", PORT)
 
+# Aggregated event counters
+latency_events = 0
+failure_events = 0
+performance_events = 0
+
 while True:
+
     data, addr = sock.recvfrom(1024)
+
     message = data.decode()
+
     node, event, value = message.split("|")
 
+    # Event classification + aggregation
+    if event == "LATENCY_HIGH":
+        latency_events += 1
+
+    # Display event info
     print("Node:", node)
     print("Event:", event)
     print("Value:", value)
-    print("Message:", message)
-    print()
+
+    # Aggregated visualization
+    print("Total Latency Alerts:", latency_events)
+    print("Total Failure Events:", failure_events)
+    print("Total Performance Events:", performance_events)
+
+    print("----------------------------------")
+
+    # Packet loss tolerance (ACK reply)
+    sock.sendto(b"ACK", addr)
